57ab0119c7c1704615b24d7330de5592fcce28f6
Umut Sever <severumut@yahoo.com>
2021-04-28 11:13:46 +0300

    Initial Commit

---
+++ Lyft-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lyft-Clone
+//
+//  Created by Umut Sever on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
