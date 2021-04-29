94fe80422ac267b85723e928c83cc3a716b03bf9
Umut Sever <severumut@yahoo.com>
2021-04-29 11:35:49 +0300

    User Location with CoreLocation and MapKit Implemented

--- Lyft-Clone/Controller/HomeViewController.swift
+++ Lyft-Clone/Controller/HomeViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class HomeViewController: UIViewController, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDataSource, CLLocationManagerDelegate, MKMapViewDelegate {
     
     
     
     @IBOutlet weak var searchButton: UIButton!
     
     var locations = [Location]()
+    var locationManager: CLLocationManager!
+    var currentUserLocation: Location!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +27,37 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         let recentLocations = LocationService.shared.getRecentLocations()
         locations = [recentLocations[0], recentLocations[1]]
         
+        
+        //
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        if locationManager.authorizationStatus == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+        
         //Add shadow to searchButton
         searchButton.layer.cornerRadius = 10.0
         searchButton.layer.shadowRadius = 1.0
         searchButton.layer.shadowColor = UIColor.black.cgColor
         searchButton.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
         searchButton.layer.shadowOpacity = 0.5
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let firstLocation = locations.first!
+        currentUserLocation = Location(title: "Current Location", subtitle: "", lat: firstLocation.coordinate.latitude, lng: firstLocation.coordinate.longitude)
+        locationManager.stopUpdatingLocation()
+    }
+    
+   
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
     }
     
 
@@ -42,6 +72,14 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         let location = locations[indexPath.row]
         cell.update(location: location)
         return cell
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        //zoom in to the user location
+        let distance = 200.0
+        let region = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: distance, longitudinalMeters: distance)
+        mapView.setRegion(region, animated: true)
     }
 
 }
